c1d191bb05eaa139e1e79b84b70c1daa9a32ac1f
EDY150423 <edgarisai.ramirezcontreras.gen23@cbtis105.edu.mx>
2025-06-10 10:24:59 -0600

    Dia 2

--- Controllers/MiProyectoController.cs
+++ Controllers/MiProyectoController.cs
@@ -18,10 +18,22 @@ public class MiProyectoController : ControllerBase
             proyecto = "Configuración de Proyecto y Backend",
             integrante1 = "Edgar Ramirez",
             integrante2 = "Abish Ariana",
-            fecha = new DateTime(2025, 6, 5)
+            fecha = new DateTime(2025, 6, 8)
         };
 
         return Ok(proyecto);
-    
-}
+
+    }
+    [HttpGet("presentacion")]
+    public IActionResult Presentacion()
+    {
+        var client = new MongoClient(CadenasConexion.Mongo_DB);
+        var database = client.GetDatabase("Escuela_Edgar_Abish");
+        var collection = database.GetCollection<Equipo>("Equipo");
+
+        var filter = FilterDefinition<Equipo>.Empty;
+        var item = collection.Find(filter).FirstOrDefault();
+
+        return Ok(item);
+    }
 }
