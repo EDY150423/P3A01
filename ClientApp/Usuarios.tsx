64bfab4e130c03ab5d17e9f33e156b0daa4f5351
EDY150423 <edgarisai.ramirezcontreras.gen23@cbtis105.edu.mx>
2025-06-13 09:40:06 -0600

    dia 5

--- ClientApp/Usuarios.tsx
+++ ClientApp/Usuarios.tsx
@@ -1,4 +1,57 @@
+import { useEffect, useState } from "react";
+
+type Usuario = {
+    "Id": string,
+    "Nombre": string,
+    "Password": string,
+    "Correo": string
+}
 const Usuarios = () =>{
+    //Datos
+    const [registros, setRegistros ] = useState<Usuario[]>([]);
+    const [texto, setTexto] = useState("");
+
+    const listarRegistros = async () => {
+        let url = "/api/usuarios";
+        
+        if(texto) {
+            url += "?texto=" + texto;
+        }
+        
+        const resp = await fetch(url);
+        if(resp.ok){
+            const datos = await resp.json();
+            setRegistros(datos);
+        }
+    }
+
+    useEffect(()=>{
+        listarRegistros();
+    }, []);
+
+    const buscar = () => {
+        listarRegistros();
+    }
+
+    const eliminar = async (item: Usuario) => {
+        if(!confirm("¿Desea eliminar el usuario" + item.Nombre + "?")){
+            return;
+        }
+        const resp = await fetch("/api/usuarios/" + item.Id, {
+            method: "DELETE"
+        });
+
+        if(resp.ok){
+            listarRegistros();
+        }
+        else {
+            const error = await resp.text();
+            console.log(error);
+            alert("Ocurrió un error al eliminar el registro")
+        }
+    }
+
+    //Vista
     return (
         <>
         <div className="container">
@@ -12,16 +65,65 @@ const Usuarios = () =>{
                         <div className="col-12">
                             <div className="mb-3">
                                 <label>Búsqueda de Usuarios</label>
-                                <input type="text" className="form-control" placeholder="Introduce el nombre o el correo"/>
+                                <input type="text" className="form-control" 
+                                placeholder="Introduce el nombre o el correo"
+                                onChange={(e) => setTexto(e.target.value)}/>
                             </div>
                         </div>
-                        </div className="col-12">
-                            <button className="btn btn-primary">Buscar</button>
+                        <div className="col-12">
+                            <button className="btn btn-primary" onClick={buscar}>Buscar</button>
                         </div>
                     </div>
+                </div>
+            </div>
+        </div>
+
+        <div className="container">
+            <div className="card">
+                <div className="card-header">Usuarios existentes</div>
+                <div className="card-body">
+                    <table className="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>No.</th>
+                                <th>Nombre</th>
+                                <th>Correo</th>
+                                <th>Password</th>
+                            </tr>
+                        </thead>
+                        {
+                            registros.length === 0 &&
+                            <tbody>
+                                <tr>
+                                    <td colSpan={5}>No hay registros para mostrar</td>
+                                </tr>
+                            </tbody>
+                        }
+                        {
+                            registros.length > 0 &&
+                            <tbody>
+                                {
+                                    registros.map((item, index) =>
+                                    <tr>
+                                        <td>{index + 1}</td>
+                                        <td>{item.Nombre}</td>
+                                        <td>{item.Correo}</td>
+                                        <td>{item.Password}</td>
+                                        <td className="d-flex gap-2">
+                                            <a className="btn btn-primary" href={"/usuarios/" + item.Id}>Editar</a> 
+                                            <button className="btn btn-danger" onClick={()=> eliminar(item)}>Eliminar</button>
+                                        </td>                
+                                    </tr>
+                                    )
+                                }
+                            </tbody>
+                        }
+                    </table>
                 </div>
             </div>
         </div>
         </>
     )
 }
+
+export default Usuarios;
--- Controllers/Api/ApiUsuariosController.cs
+++ Controllers/Api/ApiUsuariosController.cs
@@ -16,14 +16,34 @@ public class ApiUsuariosController : ControllerBase
     public ApiUsuariosController()
     {
         var client = new MongoClient(CadenasConexion.Mongo_DB);
-        var database = client.GetDatabase("Parcial3_Escuela");
+        var database = client.GetDatabase("Escuela_Edgar_Abish");
         this.collection = database.GetCollection<Usuario>("Usuarios");
     }
     [HttpGet]
-    public IActionResult ListarUsuarios()
+    public IActionResult ListarUsuarios(string? texto)
     {
         var filter = FilterDefinition<Usuario>.Empty;
+        if (!string.IsNullOrWhiteSpace(texto))
+        {
+            var filterNombre = Builders<Usuario>.Filter.Regex(u => u.Nombre, new BsonRegularExpression(texto, "i"));
+            var filterCorreo = Builders<Usuario>.Filter.Regex(u => u.Correo, new BsonRegularExpression(texto, "i"));
+            filter = Builders<Usuario>.Filter.Or(filterNombre, filterCorreo);
+        }
+
         var list = this.collection.Find(filter).ToList();
+
         return Ok(list);
+    }
+    [HttpDelete("{id}")]
+    public IActionResult Delete(string id)
+    {
+        var filter = Builders<Usuario>.Filter.Eq(x => x.Id, id);
+        var item = this.collection.Find(filter).FirstOrDefault();
+        if (item != null)
+        {
+            this.collection.DeleteOne(filter);
+        }
+
+        return NoContent();
     }
 }
